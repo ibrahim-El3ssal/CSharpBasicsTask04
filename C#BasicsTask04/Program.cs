155224574800613ede49e793de5584067427724f
dell <dell@DESKTOP-NOFGJ17>
2026-09-14 12:02:42 +0300

    Add project files.

---
+++ C#BasicsTask04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_BasicsTask04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
