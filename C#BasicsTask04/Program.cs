6a75b44a464d0c40b4bda325e74338403d77903c
ibrahim-El3ssal <ibrahimelassal662@gmail.com>
2026-09-14 12:06:20 +0300

    Question 02

--- C#BasicsTask04/Program.cs
+++ C#BasicsTask04/Program.cs
@@ -10,6 +10,20 @@
             double[] itemPrices = new double[] { 25.5, 40.0, 33.75 };
             Console.WriteLine(itemPrices[1]);
             #endregion
+
+            #region Question 02  
+            // 2 - Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+            // shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
+            Console.WriteLine("\nQuestion02 : ");
+            int[,] shelfCopies = new int[,]
+            {
+                { 3, 5 },
+                { 1, 4 }
+            };
+            Console.WriteLine(shelfCopies[1, 0]);
+            #endregion
+
+
         }
     }
 }
